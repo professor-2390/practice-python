b4ef5db788a1cf93f4d7b9fae7d47dd5ce980fa9
Professor <r0t@null>
2025-04-20 20:52:34 +0100

    Day 2

---
+++ 101/01-Basics/numbers.py
@@ -0,0 +1,41 @@
+t1_int = 1
+t1_float = 1.0
+
+print(t1_int)
+print(t1_float)
+
+print(type(t1_int))
+print(type(t1_float))
+
+t1_float = 1.0
+
+t1_complex = 3.14j
+print(t1_complex)
+print(type(t1_complex))
+
+t1_hex = 0xa
+print(t1_hex)
+print(type(t1_hex))
+
+t1_octal = 0o10
+print(t1_octal)
+print(type(t1_octal))
+
+# you can add simple number with octal and hex
+print(1 + 0x1 + 0o1)
+
+# absolute value (distance between that number and 0)
+print(abs(3))
+print(abs(-3))
+
+# round number to an integer
+print(round(1.4))
+print(round(1.55))
+print(round(1.8))
+
+# binary representation
+print(bin(0))
+
+# hex representation
+print(hex(0))
+
